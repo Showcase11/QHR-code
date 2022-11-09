f849984603dbc7c61a4a28d26c42c23cf2017e89
sukanya055 <106137610+sukanya055@users.noreply.github.com>
2022-11-09 17:10:44 +0530

    updated employee page

---
+++ src/components/ManageEmployee/Editjobinfo.jsx
@@ -0,0 +1,183 @@
+import React from 'react';
+import Img1 from '../../Images/portrait-cheerful-attractive-young-woman-longsleeve-standing-with-arms-crossed-smiling.jpg';
+
+const Editjobinfo = () => {
+    return (
+        <div>
+            <input type="checkbox" id="editjobinfo" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box w-11/12 max-w-3xl">
+    <label htmlFor="editjobinfo" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+    <h2 className="font-bold text-lg pb-3">Edit Job Information</h2><hr className='pb-5' />
+    <form className="w-full">
+           <div className='flow-root'>
+           <div className="flex w-12 rounded pb-5">
+    <img
+src={Img1}
+alt="profile"
+className="mask rounded-full w-12 h-12"
+/>
+    <h1 className='flex flex-col pl-5 pt-3'>
+              Sukanya
+  </h1>
+    </div>
+           <div className="mb-2 w-[49%]">
+                        <label
+                            for="lastname"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Effective Date*
+                        </label>
+                        <input
+                            type="date"
+                            className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:outline-none focus:ring focus:ring-opacity-40"
+                            placeholder='Last Name'
+                            value='113-865'
+                        />
+                    </div>  
+           <div className="mb-2 pb-5">
+                        <label
+                            for="firstname"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                           Note
+                        </label>
+                        <textarea
+                            type="text"
+                            className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:outline-none focus:ring focus:ring-opacity-40"
+                            placeholder='Note'
+                        />
+                    </div>
+                    <h1 className='font-bold pb-5'>Job Information</h1>
+  <div className='float-left w-[49%]'>
+  
+                    <div className="mb-2">
+                        <label
+                            for="lastname"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Job Title*
+                        </label>
+                        <select className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:outline-none focus:ring focus:ring-opacity-40" 
+                         placeholder="Nationality"
+                         >
+                              <option>Select Job Title</option>
+                              <option>Account Executive</option>
+                              <option>Account Manager</option>
+                              <option>Admin</option>
+                              <option>Admin Manager</option>
+                              <option>Finanace Manager</option>
+                              <option>Finanace Specialist</option>
+                              <option>Hr Manager</option>
+                              <option>Hr Specialist</option>
+                        </select>
+                    </div>
+                    <div className="mb-2">
+                        <label
+                            for="dob"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Line Manager
+                        </label>
+                        <select className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:outline-none focus:ring focus:ring-opacity-40" 
+                         placeholder="Line Manager"
+                         >
+                              <option>Select Line Manager</option>
+                              <option>Sukanya</option>
+                              <option>Sahithya</option>
+                              <option>Jahnavi</option>
+                              <option>Rahul</option>
+                              <option>Ibrahim</option>
+                              <option>Shourya</option>
+                        </select>
+                    </div>
+                    <div className="mb-2">
+                        <label
+                            for="dob"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Department*
+                        </label>
+                        <select className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:outline-none focus:ring focus:ring-opacity-40" 
+                         placeholder="Department"
+                         >
+                              <option>Select Department</option>
+                              <option>Finance</option>
+                              <option>HR</option>
+                              <option>IT</option>
+                              <option>Management</option>
+                              <option>Marketing</option>
+                              <option>Operations</option>
+                              <option>Qurinom Solutions</option>
+                              <option>Sales</option>
+                        </select>
+                    </div>
+                    
+  </div>
+  <div className='float-right w-[49%]'>
+  <div className="mb-2">
+                        <label
+                            for="gender"
+                           className="block text-sm font-semibold text-gray-800"
+                        >
+                            Position Type*
+                        </label>
+                        <select className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:outline-none focus:ring focus:ring-opacity-40" 
+                         placeholder="Nationality"
+                         >
+                              <option>Select Position Type</option>
+                              <option>Employee</option>
+                              <option>Manager</option>
+                        </select>
+                    </div>
+                    <div className="mb-2">
+                        <label
+                            for="dob"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Employment Type
+                        </label>
+                        <select className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:outline-none focus:ring focus:ring-opacity-40" 
+                         placeholder="Employment Type"
+                         >
+                              <option>Select Employment Type</option>
+                              <option>Contractor</option>
+                              <option>Full-time</option>
+                              <option>Intern</option>
+                              <option>Part-time</option>
+                              <option>Freelancer</option>
+                        </select>
+                    </div>
+                    <div className="mb-2">
+                        <label
+                            for="dob"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Office*
+                        </label>
+                        <select className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:outline-none focus:ring focus:ring-opacity-40" 
+                         placeholder="Office"
+                         >
+                              <option>Select  Office</option>
+                              <option>Qurinom Sloutions</option>
+                        </select>
+                    </div>
+                    
+  </div>
+  </div>
+  <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+<div className="rounded-md shadow">
+ <button value="Submit" className="flex w-full items-center justify-center rounded-md border border-transparent bg-green-400 px-3 py-2 text-base font-bold text-white hover:bg-green-500  md:px-5 text-sm">Save</button>
+</div>
+<div className="mt-3 sm:mt-0 sm:ml-3">
+ <button className="flex w-full items-center justify-center rounded-md border border-transparent bg-slate-300 px-3 py-2 text-base font-bold text-slate-600 hover:bg-slate-400 md:px-5 text-lg">Cancel</button>
+</div>
+</div>
+</form>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Editjobinfo;
--- src/components/ManageEmployee/empprofile.jsx
+++ src/components/ManageEmployee/empprofile.jsx
@@ -7,16 +7,22 @@ import { IoGlobeOutline } from "react-icons/io5";
 import {Link} from 'react-router-dom';
 import { IoPersonCircleOutline } from "react-icons/io5";
 import { IoInformationCircleOutline } from "react-icons/io5";
+import General from './General';
+import Job from './Job';
 
-const empprofile = () => {
-      
+const Empprofile = () => {
+  const [openTab, setOpenTab] = useState(1);
+  // useEffect(() => {
+
+  // })
+
     return (
-        <div className='flex bg-zinc-100 w-full p-14'>
-        <div class="grid grid-rows-3 grid-flow-col gap-4">
-        <div class="row-span-3 flex card card-side bg-base-100 shadow-xl  pl-10 pr-8 pt-10">
-                <div>
+      <div className="flex p-12 bg-zinc-100">
+            <div className="flex flex-col h-[1008px] p-3 bg-base-100 border w-1/5 mr-6">
+            <div className='p-7'>
         <img
 src={Img1}
+
 alt="profile"
 className="mask rounded-full w-28 h-28 mb-4"
 />
@@ -26,7 +32,7 @@ className="mask rounded-full w-28 h-28 mb-4"
           <option>Onboarding</option>
           <option>proationary</option>
           <option>On leave</option>
-        </select>
+</select>
 
         <div className='font-bold mb-5'>-</div>
         <h2 className='pb-5 text-gray-400'>HR Manager</h2><hr className='pb-5' />
@@ -40,51 +46,50 @@ className="mask rounded-full w-28 h-28 mb-4"
     <li><label htmlFor="offboardemp">Offboarding</label></li>
   </ul>
  </div>
- {/* offboard */}
  <input type="checkbox" id="offboardemp" className="modal-toggle" />
 <div className="modal">
 <div className="modal-box  w-11/12 max-w-3xl">
     <label htmlFor="offboardemp" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
     <h1 className="font-bold text-2xl pb-3">Offboard Employee</h1><hr className='pb-5' />
-    <div class='flex items-center px-2 py-3'>
-            <form class="w-full">
-  <div class="md:flex md:items-center mb-6">
-    <div class="w-1/4">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+    <div className='flex items-center px-2 py-3'>
+            <form className="w-full">
+  <div className="md:flex md:items-center mb-6">
+    <div className="w-1/4">
+      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
       Employee*
       </label>
     </div>
-    <div class="w-3/4">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Aoi Nakanishi" />
+    <div className="w-3/4">
+      <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Aoi Nakanishi" />
     </div>
   </div>
-  <div class="md:flex md:items-center mb-6">
-    <div class="w-1/4">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name" >
+  <div className="md:flex md:items-center mb-6">
+    <div className="w-1/4">
+      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name" >
       Resignation Date*
       </label>
     </div>
-    <div class="w-3/4">
+    <div className="w-3/4">
     <input type='date' className="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" placeholder="Select Date" />
     </div>
   </div>
-  <div class="md:flex md:items-center mb-6">
-    <div class="w-1/4">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+  <div className="md:flex md:items-center mb-6">
+    <div className="w-1/4">
+      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
       Last Working Date*
       </label>
     </div>
-    <div class="w-3/4">
+    <div className="w-3/4">
     <input type='date' className="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" placeholder="Select Date" />
     </div>
   </div>
-  <div class="md:flex md:items-center mb-6">
-    <div class="w-1/4">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+  <div className="md:flex md:items-center mb-6">
+    <div className="w-1/4">
+      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
       Reason for Leaving*
       </label>
     </div>
-    <div class="w-3/4">
+    <div className="w-3/4">
     <select className="select text-gray-500 w-full border-inherit">
                                  <option>Select Reason For Leaving</option>
                                  <option>Contract Finished</option>
@@ -96,13 +101,13 @@ className="mask rounded-full w-28 h-28 mb-4"
                         </select>
     </div>
   </div>
-  <div class="md:flex md:items-center mb-6">
-    <div class="w-1/4">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+  <div className="md:flex md:items-center mb-6">
+    <div className="w-1/4">
+      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
       Manager Reassignment*
       </label>
     </div>
-    <div class="w-3/4">
+    <div className="w-3/4">
     <select className="select text-gray-500 w-full border-inherit">
                                  <option>Select Employee</option>
                                  <option>Sukanya <p>sukanya@grovehr.com</p></option>
@@ -114,14 +119,14 @@ className="mask rounded-full w-28 h-28 mb-4"
                         </select>
     </div>
   </div>
-  <div class="md:flex md:items-center mb-6">
-    <div class="w-1/4">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+  <div className="md:flex md:items-center mb-6">
+    <div className="w-1/4">
+      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
       Note
       </label>
     </div>
-    <div class="w-3/4">
-      <textarea class="h-36 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Note" />
+    <div className="w-3/4">
+      <textarea className="h-36 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Note" />
     </div>
   </div>
   
@@ -142,16 +147,14 @@ className="mask rounded-full w-28 h-28 mb-4"
 </div>
   </div>
 </div>
-{/* onboard */}
  <input type="checkbox" id="my-modal-3" className="modal-toggle" />
 <div className="modal">
 <div className="modal-box  w-11/12 max-w-3xl">
     <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
     <h2 className="font-bold text-lg pb-3">Assign Checklist</h2><hr className='pb-5' />
 
-<div class="flow-root pb-7 gap-3">
-
-<p class="float-left w-2/5">
+<div className="flow-root pb-7 gap-3">
+<p className="float-left w-2/5">
 <h3 className='flex pb-5'><IoPersonCircleOutline className='text-2xl' />Sukanya</h3>
 
           <p className='pb-3 font-bold'>Checklist Template *</p>
@@ -179,7 +182,6 @@ className="mask rounded-full w-28 h-28 mb-4"
                    <option>Sukanya B boorlesukanya@gmail.com</option>
               </select>
 </p>
-
 </div>
 <p className='flex'><IoInformationCircleOutline className='text-xl text-zinc-400 mr-2' />We will send reminders to employee about incomplete tasks.</p>
 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
@@ -199,16 +201,15 @@ className="mask rounded-full w-28 h-28 mb-4"
 <h2 className='text-gray-400'>LINE MANAGER</h2>
 <div>@ Sukanya </div>
 </div>
-        </div>
-        <div className=" mx-auto">
-        <div className="flex flex-col justify-center">
-            <div className="bg-white w-full shadow-lg">
-        <div className="px-8 py-3 gap-1 font-sans flex items-center shadow-lg justify-between font-bold relative text-sm">
-          <ul className="flex space-x-2 justify-center uppercase">
+            </div>
+            <div className="container w-3/4">
+                <div className="grid grid-cols-1 gap-6 mb-6 lg:grid-cols-1">
+                    <div className="w-full py-3 bg-base-100 border">
+                    <ul className="flex space-x-2 uppercase">
                         <li>
                             <Link
-                                href="/todos"
-                                // onClick={() => setOpenTab(1)}
+                                to='/employee/general'
+                                onClick={() => setOpenTab(1)}
                                 className="inline-block px-4 py-2 text-gray-600 rounded bordered active underline underline-offset-8"
                             >
                                 GENERAL
@@ -216,64 +217,63 @@ className="mask rounded-full w-28 h-28 mb-4"
                         </li>
                         <li>
                             <Link
-                                to="#"
-                                // onClick={() => setOpenTab(2)}
-                                className="inline-block px-4 py-2 text-gray-600 rounded underline underline-offset-8"
+                                to="/employee/job"
+                                onClick={() => setOpenTab(2)}
+                                className="inline-block px-4 py-2 text-gray-600 rounded underline-offset-8"
                             >
                                 JOB
                             </Link>
                         </li>
                         <li>
                             <Link
-                                to="#"
-                                // onClick={() => setOpenTab(3)}
-                                className="inline-block px-4 py-2 text-gray-600 rounded underline underline-offset-8"
+                                to="/employee/payroll"
+                                onClick={() => setOpenTab(3)}
+                                className="inline-block px-4 py-2 text-gray-600 rounded underline-offset-8"
                             >
                                 PAYROLL
                             </Link>
                         </li>
                         <li>
                             <Link
-                                to="#"
-                                // onClick={() => setOpenTab(4)}
-                                className="inline-block px-4 py-2 text-gray-600 rounded underline underline-offset-8"
+                                to="/employee/documents"
+                                onClick={() => setOpenTab(4)}
+                                className="inline-block px-4 py-2 text-gray-600 rounded underline-offset-8"
                             >
                                 DOCUMENTS
                             </Link>
                         </li>
                         <li>
                             <Link
-                                to="#"
-                                // onClick={() => setOpenTab(4)}
-                                className="inline-block px-4 py-2 text-gray-600 rounded underline underline-offset-8"
+                                to="/employee/dependends"
+                                onClick={() => setOpenTab(4)}
+                                className="inline-block px-4 py-2 text-gray-600 rounded underline-offset-8"
                             >
-                                DEPENDENTS
+                                DEPENDENDS
                             </Link>
                         </li>
                         <li>
                             <Link
-                                to="#"
-                                // onClick={() => setOpenTab(4)}
-                                className="inline-block px-4 py-2 text-gray-600 rounded underline underline-offset-8"
+                                to="/employee/accountsettings"
+                                onClick={() => setOpenTab(4)}
+                                className="inline-block px-4 py-2 text-gray-600 rounded underline-offset-8"
                             >
                                 ACCOUNT SETTINGS
                             </Link>
                         </li>
                     </ul>
-        </div>
-        
-      </div>
+                    </div>
+                    <div className={openTab === 1 ? "block" : "hidden"}>
+                            {" "}
+                           <General />
+                        </div>
+                    <div className={openTab === 2 ? "block" : "hidden"}>
+                          {" "}
+                          <Job />
+                    </div>
                 </div>
             </div>
-       
-        <div class="tile bg-base-100 row-start-4 row-end-5 md:row-start-2 md:row-end-3 col-span-2">
-         jgujgkuhkjnknkj
- sfsfsdfkjhcjkshds
-fdasdbshjbjdkjsfd
         </div>
-      </div>
-       </div>
     );
 };
 
-export default empprofile;
+export default Empprofile;
